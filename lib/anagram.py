6411a3ee36728b712e6ce66f11dcb5f66da5e692
Douglas Gatimu <douglasgatimu@gmail.com>
2025-07-13 02:35:02 +0300

    match() function for anagram added.

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+from collections import Counter
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        return [word_ for word_ in word_list if Counter(self.word) == Counter(word_)]
+
+
+
